a330bcb48980b2e50f2be933494cb324dc783b3e
Derek Herman <derek@valendesigns.com>
2014-02-11 20:20:51 -0800

    update version number

--- header.php
+++ header.php
@@ -25,13 +25,13 @@
     
     <p class="note note-danger"><?php printf( __( '%s needs to be installed! Choose an installation mode, and follow the steps below to get everything setup.', 'option-tree-theme' ), '<a href="http://wordpress.org/plugins/option-tree/" target="_blank">' . __( 'OptionTree', 'option-tree-theme' ) . '</a>' ); ?></p>
   
-  <?php } else if ( defined( 'OT_VERSION' ) && version_compare( OT_VERSION, '2.3.0', '<' ) ) { ?>
+  <?php } else if ( defined( 'OT_VERSION' ) && version_compare( OT_VERSION, '2.3.1', '<' ) ) { ?>
     
-    <p class="note note-danger"><?php printf( __( 'Please upgrade! This theme requires OptionTree %s or newer and you have %s installed.', 'option-tree-theme' ), '<small>2.3.0</small>', '<small>' . OT_VERSION . '</small>' ); ?></p>
+    <p class="note note-danger"><?php printf( __( 'Please upgrade! This theme requires OptionTree %s or newer and you have %s installed.', 'option-tree-theme' ), '<small>2.3.1</small>', '<small>' . OT_VERSION . '</small>' ); ?></p>
   
   <?php } else if ( defined( 'OT_VERSION' ) ) { ?>
     
-    <p class="note"><?php printf( __( 'Congratulations! You\'ve successfully installed OptionTree %s.', 'option-tree-theme' ), '<small>2.3.0</small>', '<small>' . OT_VERSION . '</small>' ); ?></p>
+    <p class="note"><?php printf( __( 'Congratulations! You\'ve successfully installed OptionTree %s.', 'option-tree-theme' ), '<small>' . OT_VERSION . '</small>' ); ?></p>
     
   <?php } ?>
   
